ff44423d3fc819e55a71c20337c39966ae6e13c0
Charlie Steele <steelech@umich.edu>
2016-09-07 23:48:12 -0400

    Styling for trip-select and route-select buttons

--- ember/app/components/route-select.js
+++ ember/app/components/route-select.js
@@ -7,22 +7,21 @@ export default Ember.Component.extend({
 	overListElement: false,
 	buttonClasses: "get-route-button",
 	buttonClass: "hidden",
+	iconClasses: '',
+	iconClass: "hidden",
 	disabled: false,
 	direction: '',
 	route_name: '',
 	actions: {
 		over: function() {
-			this.set("buttonClass", this.get("buttonClasses"));
+			this.set("iconClass", this.get("iconClasses"));
 		},
 		off: function() {
-			this.set("buttonClass", "hidden");
+			this.set("iconClass", "hidden");
 		},
 		selectRoute() {
 			this.sendAction("selectRoute", this.get("route_name"), this.get("id"));
 
 		},
-		setRoute: function(param) {
-			this.set('route_name', param);
-		}
 	}
 });
--- ember/app/components/trip-select.js
+++ ember/app/components/trip-select.js
@@ -4,16 +4,16 @@ export default Ember.Component.extend({
 	direction: '',
 	trip_name: '',
 	id: '',
-	buttonClasses: "btn btn-primary",
-	buttonClass: "hidden",
+	iconClasses: "",
+	iconClass: "hidden",
 	overListElement: false,
 	disabled: false,
 	actions: {
 		over: function() {
-			this.set("buttonClass", this.get("buttonClasses"));
+			this.set("iconClass", this.get("iconClasses"));
 		},
 		off: function() {
-			this.set("buttonClass", "hidden");
+			this.set("iconClass", "hidden");
 		},
 		setTrip: function(param) {
 			this.sendAction("setTrip", this.get('trip_name'), this.get('id'));
